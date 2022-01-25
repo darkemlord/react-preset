3c97685177804cd9db6a4e9d21db0e4d051941e4
emanuel caro <darkemlord@gmail.com>
2022-01-25 17:41:23 +0900

    login routes in express created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const loginRoutes = require('./routes/login')
 
 const app = express();
+
+app.use('/login', loginRoutes)
 
 app.listen(4000, () => {
   console.log(`server running at http://localhost:4000`)
