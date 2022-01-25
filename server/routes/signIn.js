43c2fab3b23e1abd14752c8fd46d4971f666a369
emanuel caro <darkemlord@gmail.com>
2022-01-25 23:31:03 +0900

    encrypting passwords

--- server/routes/signIn.js
+++ server/routes/signIn.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const User = require('../models/Users')
+const User = require('../models/Users');
+const bcrypt = require('bcrypt')
 
 const signInRoutes = express.Router();
 
 signInRoutes.post('/', async (req, res) => {
+  const password = req.body.password;
+  const hash = await bcrypt.hash(password, 10);
   const user = new User({
     userName: req.body.userName,
     email: req.body.email,
-    password: req.body.password
+    password: hash
   });
   try{
     const saveUser = await user.save();
