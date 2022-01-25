6d3c40f1bc8b9685ca7e9d538d35da9bd15ee5e3
emanuel caro <darkemlord@gmail.com>
2022-01-25 18:06:18 +0900

    user post route updated

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const User = require('../models/Users')
 
 const loginRoutes = express.Router();
 
-loginRoutes.get('/', (req, res) => {
-  res.send('Hola mis nenes desde login')
+loginRoutes.post('/', async (req, res) => {
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password
+  })
+  try{
+    const saveUser = await user.save();
+    res.json(saveUser)
+  } catch(error) {
+    res.json({ message: error })
+  }
 })
 
 module.exports = loginRoutes;
