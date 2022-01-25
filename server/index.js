4161c471c4271663333bc73bb6cd12f731acd83c
emanuel caro <darkemlord@gmail.com>
2022-01-25 17:52:59 +0900

    mongo db database connected

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+dotenv.config({ path: ".env" })
 const app = express();
-
+app.use(cors())
 app.use('/login', loginRoutes)
 
-app.listen(4000, () => {
-  console.log(`server running at http://localhost:4000`)
+
+
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, _) => {
+  if(err) {
+    console.log('connection error');
+    console.log(err)
+  } else {
+    app.listen(4000, () => {
+      console.log(`server running at http://localhost:4000`)
+    })
+  }
 })
