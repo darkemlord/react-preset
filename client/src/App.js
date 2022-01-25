471204a200e1626aaa10e61797e83ea7fbf005cc
emanuel caro <darkemlord@gmail.com>
2022-01-25 17:33:38 +0900

    removing modules

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>hello from react!</h1>
+      <h1 className="container">hello from react!</h1>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+
+const app = use.express();
+
+app.listen(4000, () => {
+  console.log(`server running at http://localhost:4000`)
+})
